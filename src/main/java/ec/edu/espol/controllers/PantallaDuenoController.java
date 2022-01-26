3375c07007987f55b56027704a8d1963715f4042
unknown <ricluper@espol.edu.ec>
2022-01-26 12:28:23 -0500

    Anteriormente no se envio la funcion limpiar y regresar

--- src/main/java/ec/edu/espol/controllers/PantallaDuenoController.java
+++ src/main/java/ec/edu/espol/controllers/PantallaDuenoController.java
@@ -5,13 +5,23 @@
  */
 package ec.edu.espol.controllers;
 
+import ec.edu.espol.proyectopoo.App;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -47,10 +57,32 @@ public class PantallaDuenoController implements Initializable {
 
     @FXML
     private void regresarPrincipal(MouseEvent event) {
+        try {
+            Stage stg = (Stage)btnRegresar.getScene().getWindow();
+            stg.close();
+            FXMLLoader loader = App.loadFXML("pantallaInicial");
+            Scene sc = new Scene(loader.load(),600,400);
+            Stage sg = new Stage();
+            sg.setScene(sc);
+            sg.setTitle("Concurso de Mascotas");
+            String rut = System.getProperty("user.dir") + "/src/main/resources/img/icono.png";
+            Path ruta = Paths.get(rut);
+            Image imagen = new Image("file:" + ruta);
+            sg.getIcons().add(imagen);
+            sg.show();
+        } catch (IOException ex) {
+            Alert a = new Alert(AlertType.ERROR, "No es posible regresar a la ventana principal");
+            a.show();
+        }
     }
 
     @FXML
     private void limpiar(MouseEvent event) {
+        infNombres.setText("");
+        infApellidos.setText("");
+        infDireccion.setText("");
+        infTelefono.setText("");
+        infEmail.setText("");
     }
     
 }
--- src/main/java/ec/edu/espol/controllers/PantallaInicialController.java
+++ src/main/java/ec/edu/espol/controllers/PantallaInicialController.java
@@ -5,12 +5,21 @@
  */
 package ec.edu.espol.controllers;
 
+import ec.edu.espol.proyectopoo.App;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -46,6 +55,23 @@ public class PantallaInicialController implements Initializable {
 
     @FXML
     private void accesoDueno(MouseEvent event) {
+        try {
+            Stage stg = (Stage)btnDueno.getScene().getWindow();
+            stg.close();
+            FXMLLoader loader = App.loadFXML("pantallaDueno");
+            Scene sc = new Scene(loader.load(),600,400);
+            Stage sg = new Stage();
+            sg.setScene(sc);
+            sg.setTitle("Registrar Dueño");
+            String rut = System.getProperty("user.dir") + "/src/main/resources/img/icono.png";
+            Path ruta = Paths.get(rut);
+            Image imagen = new Image("file:" + ruta);
+            sg.getIcons().add(imagen);
+            sg.show();
+        } catch (IOException ex) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "No es posible registrar un nuevo Dueño");
+            a.show();
+        }
     }
 
     @FXML
