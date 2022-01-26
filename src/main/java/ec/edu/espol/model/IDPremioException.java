3b42aaa603bebe64bbb63358dd6f239a81deabaa
Issac-Maza <issamaza@espol.edu.ec>
2022-01-25 22:13:39 -0500

    Creando IdPremioException

---
+++ src/main/java/ec/edu/espol/model/IDPremioException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author Issac Maza
+ */
+public class IDPremioException extends Exception{
+    public IDPremioException(String msg){
+        super(msg);
+    }
+}
