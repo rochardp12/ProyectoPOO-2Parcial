d2b3c6bed94d0e65881b387736e34f05d62e27f3
Issac-Maza <issamaza@espol.edu.ec>
2022-01-25 19:07:42 -0500

    He agreado el paquete model e inicializado la clase Premio

---
+++ src/main/java/ec/edu/espol/model/Premio.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+import static ec.edu.espol.model.Dueno.readFromFile;
+import ec.edu.espol.util.Util;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Premio {
+    private int id;
+    private int lugar;
+    private String descripcion;
+    private int idConcurso;
+    private Concurso concurso;
+    //constructor
+    
+}
+    
+    
