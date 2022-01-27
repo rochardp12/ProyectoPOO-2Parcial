7ba00288d554c4e2abbe5479177dd43970e23ad2
RommelZamora <razamora@espol.edu.ec>
2022-01-27 07:11:05 -0500

    Actualizando clase Concurso

--- src/main/java/ec/edu/espol/model/Concurso.java
+++ src/main/java/ec/edu/espol/model/Concurso.java
@@ -5,18 +5,26 @@
  */
 package ec.edu.espol.model;
 
+import static ec.edu.espol.model.Dueno.readFromFile;
 import ec.edu.espol.util.Util;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Objects;
 import java.util.Scanner;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
- * @author ROMMEL ZAMORA
+ * @author Usuario
  */
 public class Concurso {
     private int id;
@@ -25,209 +33,202 @@ public class Concurso {
     private LocalDate fechaInscripcion;
     private LocalDate fechaCierreInscripcion;
     private String tematica;
-    private Double costo;
+    private double costoInscripcion;
     private ArrayList<Inscripcion> inscripciones;
     private ArrayList<Premio> premios;
     private ArrayList<Criterio> criterios;
-
-    public Concurso(int id, String nombre, String fecha, String fechaInscripcion, String fechaCierreInscripcion, String tematica, Double costo) {
+    //constructor
+    
+    public Concurso(int id, String nombre, LocalDate fecha, LocalDate fechaInscripcion, LocalDate fechaCierreInscripcion, String tematica, double costoInscripcion){
         this.id = id;
         this.nombre = nombre;
-        
-        this.fecha = LocalDate.parse(fecha);
-        this.fechaInscripcion = LocalDate.parse(fechaInscripcion);
-        this.fechaCierreInscripcion = LocalDate.parse(fechaCierreInscripcion);
-        
+        this.fecha = fecha;
+        this.fechaInscripcion = fechaInscripcion;
+        this.fechaCierreInscripcion = fechaCierreInscripcion;
         this.tematica = tematica;
-        this.costo = costo;
+        this.costoInscripcion = costoInscripcion;
         this.inscripciones = new ArrayList<>();
         this.premios = new ArrayList<>();
         this.criterios = new ArrayList<>();
     }
-    
 
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public LocalDate getFechaInscripcion() {
-        return fechaInscripcion;
-    }
-
-    public LocalDate getFechaCierreInscripcion() {
-        return fechaCierreInscripcion;
-    }
-
-    public String getTematica() {
-        return tematica;
-    }
-
-    public Double getCosto() {
-        return costo;
-    }
-
-    public ArrayList<Inscripcion> getInscripciones() {
-        return inscripciones;
-    }
-
-    public ArrayList<Premio> getPremios() {
-        return premios;
-    }
-
-    public ArrayList<Criterio> getCriterios() {
-        return criterios;
-    }
-    
-    //##########Setters##########
+    //setters
 
     public void setId(int id) {
-        this.id = id;
+        try{
+            if(verificarID(id) != null)
+                throw new IDConcursoException("ID existente. Ingrese una nueva");
+            this.id = id;
+        }
+        catch(IDConcursoException ex){
+            Alert a = new Alert(AlertType.ERROR, ex.getMessage());
+            a.show();
+        }
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombre){
+        if(nombre != null)
+            this.nombre = nombre;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        if(fecha != null)
+            this.fecha = fecha;
+    }
+
+    public void setFechaInscripcion(LocalDate fechaInscripcion) {
+        if(fechaInscripcion != null)
+            this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public void setFechaCierreInscripcion(LocalDate fechaCierreInscripcion) {
+        if(fechaCierreInscripcion != null)
+            this.fechaCierreInscripcion = fechaCierreInscripcion;
     }
 
     public void setTematica(String tematica) {
-        this.tematica = tematica;
+        if(tematica != null)
+            this.tematica = tematica;
     }
 
-    public void setCosto(Double costo) {
-        if (costo>=0){
-            this.costo=costo;
+    public void setCostoInscripcion(double costoInscripcion) {
+        try{
+            if(costoInscripcion < 0)
+                throw new CostoException("Ingrese un costo correcto");
+            this.costoInscripcion = costoInscripcion;
+        }
+        catch(CostoException ex){
+            Alert a = new Alert(AlertType.ERROR, ex.getMessage());
+            a.show();
         }
     }
 
-    public void setFecha(String fecha) {
-        this.fecha = LocalDate.parse(fecha);
+    public void setInscripciones(ArrayList<Inscripcion> inscripciones) {
+        if(inscripciones != null)
+            this.inscripciones = inscripciones;
     }
 
-    public void setFechaInscripcion(String fechaInscripcion) {
-        this.fechaInscripcion = LocalDate.parse(fechaInscripcion);
+    public void setPremios(ArrayList<Premio> premios) {
+        if(premios != null)
+            this.premios = premios;
     }
 
-    public void setFechaCierreInscripcion(String fechaCierreInscripcion) {
-        this.fechaCierreInscripcion = LocalDate.parse(fechaCierreInscripcion);
+    public void setCriterios(ArrayList<Criterio> criterios) {
+        if(criterios != null)
+            this.criterios = criterios;
     }
-    
-    public void addInscripcion(Inscripcion i1){
-        this.inscripciones.add(i1);
+    //getters
+
+    public int getId() {
+        return this.id;
     }
-    
-    public void addPremio(Premio p1){
-        this.premios.add(p1);
+
+    public String getNombre() {
+        return this.nombre;
     }
+
+    public LocalDate getFecha() {
+        return this.fecha;
+    }
+
+    public LocalDate getFechaInscripcion() {
+        return this.fechaInscripcion;
+    }
+
+    public LocalDate getFechaCierreInscripcion() {
+        return this.fechaCierreInscripcion;
+    }
+
+    public String getTematica() {
+        return this.tematica;
+    }
+
+    public double getCostoInscripcion() {
+        return this.costoInscripcion;
+    }
+
+    public ArrayList<Inscripcion> getInscripciones() {
+        return this.inscripciones;
+    }
+
+    public ArrayList<Premio> getPremios() {
+        return this.premios;
+    }
+
+    public ArrayList<Criterio> getCriterios() {
+        return this.criterios;
+    }
+    //comportamientos
     
-    public void addCriterio(Criterio c1){
-        this.criterios.add(c1);
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ID Concurso: ").append(this.id).append(" --> ");
+        sb.append("Nombre: ").append(this.nombre);
+        sb.append(". Fecha: ").append(this.fecha);
+        sb.append(". Tematica: ").append(this.tematica);
+        sb.append(". Costo de inscripcion: ").append(this.costoInscripcion);
+        return sb.toString();
     }
     
     @Override
-    public String toString(){
-        
-        StringBuilder sb = new StringBuilder();
-        for (Inscripcion i : this.inscripciones){
-            sb.append("Inscripcion ");
-            sb.append(i.getId());
-            sb.append(":<Id de Mascota: ");
-            sb.append(i.getIdMascotas());
-            sb.append(", Descuento: ");
-            sb.append(i.getDescuento());
-            sb.append(">, "); 
-        }
-        
-        StringBuilder sb_premio = new StringBuilder();
-        for (Premio p : this.premios){
-            sb_premio.append("Premio: ");
-            sb_premio.append(p.getId());
-            sb_premio.append(", ");
-        }
-        
-        StringBuilder sb_criterio = new StringBuilder();
-        for (Criterio c : this.criterios){
-            sb_criterio.append("Criterio: ");
-            sb_criterio.append(c.getId());
-            sb_criterio.append(",");
-        }
-        
-        return "Concurso "+this.id+" :{Nombre: "+this.nombre+", Fecha: "+this.fecha+", Fecha de inscripcion: [desde: "+this.fechaInscripcion+", hasta: "+this.fechaCierreInscripcion+"], Tematica: "+this.tematica+", Costo de Inscripcion: "+this.costo+", Inscripciones:["+sb.toString()+"], Premios:["+sb_premio.toString()+"], Criterios:["+sb_criterio.toString()+"]}";
-    }
-    
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if(obj==null)
+            return false;
+        if(this==obj)
             return true;
-        }
-        if (obj == null) {
+        if(this.getClass()!=obj.getClass())
             return false;
-        }
-        if (this.getClass() != obj.getClass()) {
+        Concurso concurso = (Concurso)obj;
+        if(!(Objects.equals(this.nombre, concurso.nombre)))
             return false;
+        return Objects.equals(this.tematica,concurso.tematica);
+    }
+    
+    public void saveFile(String nomfile){
+        try(BufferedWriter bf = new BufferedWriter(new FileWriter(nomfile,true))){
+            bf.write(this.id + "|" + this.nombre + "|" + this.fecha + "|" + this.tematica + "|" + this.fechaInscripcion + "|" + this.fechaCierreInscripcion + "|" + this.costoInscripcion + "\n");
+            Alert a = new Alert(Alert.AlertType.CONFIRMATION,"Concurso agregado con éxito");
+            a.show();
         }
-        Concurso c=(Concurso)obj;
-        return this.id==c.id;
-    }
-    
-    public static Concurso nextConcurso(Scanner sc){
-        int id_next;
-        String nombre_next;
-        String fecha_next;
-        String fechaInscripcion_next;
-        String fechaCierreInscripcion_next;
-        String tematica_next;
-        double costo_next;
-        
-        System.out.println("Ingrese el ID del Concurso:");
-        id_next=sc.nextInt();
-        System.out.println("Ingrese el nombre del Concurso:");
-        nombre_next = sc.next();
-        System.out.println("Ingrese la fecha en la que ocurrirá el Concurso:");
-        fecha_next = sc.next();
-        System.out.println("Ingrese la fecha de INICIO de las inscripciones del Concurso:");
-        fechaInscripcion_next = sc.next();
-        System.out.println("Ingrese la fecha de CIERRE de las inscripciones del Concurso:");
-        fechaCierreInscripcion_next = sc.next();
-        System.out.println("Ingrese la tematica del Concurso:");
-        tematica_next = sc.next();
-        System.out.println("Ingrese el costo del Concurso:");
-        costo_next = sc.nextDouble();
-        
-        Concurso concurso_nuevo = new Concurso(id_next,nombre_next,fecha_next,fechaInscripcion_next,fechaCierreInscripcion_next,tematica_next,costo_next);
-        return concurso_nuevo;
-    }
-    
-    //files
-    
-    public void saveFile(String nomFile){
-        try(PrintWriter pw=new PrintWriter(new FileOutputStream(new File(nomFile),true))){
-            pw.println(this.toString());
+        catch(IOException ex){
+            Alert a = new Alert(AlertType.ERROR,"No es posible registrar al concurso");
+            a.show();
         }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        } 
     }
     
-    public static ArrayList<Concurso> readFromFile(String nomFile){
-        ArrayList<Concurso> lista=new ArrayList<>();
-        try(Scanner sc=new Scanner(new File(nomFile))){
-            while(sc.hasNextLine()){
-                String linea=sc.nextLine();
-                String[] tokens=linea.split("|");
-                Concurso c=new Concurso(Integer.parseInt(tokens[0]),tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],Double.parseDouble(tokens[6]));
-                lista.add(c);
+ 
+    
+    public static ArrayList<Concurso> readFromFile(String nomfile){
+        ArrayList<Concurso> concursos = new ArrayList<>();
+        try(BufferedReader bf = new BufferedReader(new FileReader(nomfile))){
+            String linea;
+            while((linea = bf.readLine()) != null){
+                String[] arreglo = linea.split("\\|");
+                String[] fech = arreglo[2].split("-");
+                LocalDate fecha1 = LocalDate.of(Integer.parseInt(fech[0]), Integer.parseInt(fech[1]), Integer.parseInt(fech[2]));
+                String[] fechIns = arreglo[4].split("-");
+                LocalDate fecha2 = LocalDate.of(Integer.parseInt(fechIns[0]),Integer.parseInt(fechIns[1]),Integer.parseInt(fechIns[2]));
+                String[] fechCie = arreglo[5].split("-");
+                LocalDate fecha3 = LocalDate.of(Integer.parseInt(fechCie[0]),Integer.parseInt(fechCie[1]),Integer.parseInt(fechCie[2]));
+                Concurso concurso = new Concurso(Integer.parseInt(arreglo[0]), arreglo[1], fecha1, fecha2, fecha3, arreglo[3], Double.parseDouble(arreglo[6]));
+                concursos.add(concurso);
             }
         }
-        catch(Exception e){
-            System.out.println(e.getMessage());
+        catch(IOException ex){
+            Alert a = new Alert(AlertType.ERROR,"No es posible obtener a los concursos");
+            a.show();
         }
-        return lista;
+        return concursos;
+        }
+    
+    public static Concurso verificarNombre(String nombreConcurso){
+        ArrayList<Concurso> concursos = readFromFile("concursos.txt");
+        for(Concurso concurso: concursos){
+            if(Objects.equals(concurso.nombre,nombreConcurso))
+                return concurso;
+        }
+        return null;
     }
     
 }
