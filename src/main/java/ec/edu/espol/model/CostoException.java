8bbd9dc4b170390b354509e717591d98eb6ebe23
RommelZamora <razamora@espol.edu.ec>
2022-01-29 18:54:15 -0500

    Me había faltado arreglar algo

---
+++ src/main/java/ec/edu/espol/model/CostoException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author Richard
+ */
+public class CostoException extends Exception{
+    
+    public CostoException(String msg){
+        super(msg);
+    }
+}
