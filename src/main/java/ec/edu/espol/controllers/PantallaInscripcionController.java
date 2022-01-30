f2cc968fd4d293fc68024d472104524ce7836eba
RommelZamora <razamora@espol.edu.ec>
2022-01-30 17:32:35 -0500

    Añadida funcion que no se agregó en el otro commit

--- src/main/java/ec/edu/espol/controllers/PantallaInscripcionController.java
+++ src/main/java/ec/edu/espol/controllers/PantallaInscripcionController.java
@@ -5,10 +5,18 @@
  */
 package ec.edu.espol.controllers;
 
+import ec.edu.espol.model.Concurso;
+import ec.edu.espol.model.CostoException;
+import ec.edu.espol.model.FechaInvalidaException;
+import ec.edu.espol.model.Inscripcion;
+import ec.edu.espol.model.Mascota;
+import ec.edu.espol.model.NombreConcursoException;
+import ec.edu.espol.model.NombreMascotaException;
 import ec.edu.espol.model.PanelVacioException;
 import ec.edu.espol.proyectopoo.App;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -55,6 +63,42 @@ public class PantallaInscripcionController implements Initializable {
 
     @FXML
     private void enviarDatos(MouseEvent event) {
+        try{
+            if(Objects.equals(infNombre.getText(),"") || Objects.equals(infConcurso.getText(),"") || Objects.equals(infValor.getText(),"")
+                    || Objects.equals(infDia.getText(),"") || Objects.equals(infMes.getText(),"") || Objects.equals(infAn.getText(),""))
+                throw new PanelVacioException("Obligatorio llenar todos los datos");
+            if(Mascota.verificarNombre(infNombre.getText()) == null)
+                throw new NombreMascotaException("Nombre de mascota no registrado. Ingresar correctamente o registrar primero");
+            if(Concurso.verificarNombre(infConcurso.getText()) == null)
+                throw new NombreConcursoException("Nombre de concurso no registrado. Ingresar correctamente o registrar primero");
+            double valorPagar = Double.parseDouble(infValor.getText());
+            int dia = Integer.parseInt(infDia.getText());
+            int mes = Integer.parseInt(infMes.getText());
+            int an = Integer.parseInt(infAn.getText());
+            if((dia<=0) || (dia>31))
+                throw new FechaInvalidaException("Fecha incorrecta ingresada. Verificar");
+            if((mes<=0) || (mes>12))
+                throw new FechaInvalidaException("Fecha incorrecta ingresada. Verificar");
+            if(((dia >= 30)&&((mes == 2)||(mes == 02))))
+                throw new FechaInvalidaException("Fecha incorrecta ingresada. Verificar");
+            if(an < 2022)
+                throw new FechaInvalidaException("Fecha incorrecta ingresada. Verificar");
+            if(valorPagar < 0)
+                throw new CostoException("Ingrese un valor a pagar correcto");
+            Inscripcion.crearInscripcion(Mascota.verificarNombre(infNombre.getText()), Concurso.verificarNombre(infConcurso.getText()), valorPagar, LocalDate.of(an,mes,dia));
+        }
+        catch(PanelVacioException ex){
+            Alert a = new Alert(AlertType.ERROR, ex.getMessage());
+            a.show();
+        }
+        catch(NumberFormatException ex){
+            Alert a = new Alert(AlertType.ERROR, "Ingresar numeros correctos");
+            a.show();
+        }
+        catch(NombreMascotaException | NombreConcursoException | FechaInvalidaException | CostoException ex){
+            Alert a = new Alert(AlertType.ERROR, ex.getMessage());
+            a.show();
+        }
     }
 
     @FXML
