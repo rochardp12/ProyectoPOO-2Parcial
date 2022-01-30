8b1c68baab889e572c4543c5b688ce2ef9dd2ab0
unknown <ricluper@espol.edu.ec>
2022-01-30 15:46:53 -0500

    Corrigiendo ruta

--- src/main/java/ec/edu/espol/controllers/PantallaCriterioController.java
+++ src/main/java/ec/edu/espol/controllers/PantallaCriterioController.java
@@ -193,9 +193,7 @@ public class PantallaCriterioController implements Initializable {
             Stage sg = new Stage();
             sg.setScene(sc);
             sg.setTitle("Concurso de Mascotas");
-            String rut = System.getProperty("user.dir") + "/src/main/resources/img/icono.png";
-            Path ruta = Paths.get(rut);
-            Image imagen = new Image("file:" + ruta);
+            Image imagen = new Image("img\\icono.png");
             sg.getIcons().add(imagen);
             sg.show();
         } catch (IOException ex) {
